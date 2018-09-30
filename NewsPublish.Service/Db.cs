058d421e15c871fa2c813e63d484c209c4568794
Kerwin <1079091667@qq.com>
2018-09-30 11:18:40 +0800

    添加项目文件。

---
+++ NewsPublish.Service/Db.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NewsPublish.Model.Entity;
+namespace NewsPublish.Service
+{
+    /// <summary>
+    /// 数据库访问上下文
+    /// </summary>
+    public class Db : DbContext
+    {
+        public Db() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=NewsPublish;User ID=sa;Password=ckjbug",b=>b.UseRowNumberForPaging());
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public virtual DbSet<Model.Entity.Banner> Banner { get; set; }
+        public virtual DbSet<NewsClassify> NewsClassify { get; set; }
+        public virtual DbSet<News> News { get; set; }
+        public virtual DbSet<NewsComment> NewsComment { get; set; }
+    }
+}
